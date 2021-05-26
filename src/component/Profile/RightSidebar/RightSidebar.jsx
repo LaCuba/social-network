0682bf0b03f55693986f7b6126ce405ca1e35e09
LaCuba <nikolay.romanov164@gmail.com>
2021-05-26 13:34:50 +0300

    Updated RightSidebar

--- src/component/Profile/RightSidebar/RightSidebar.jsx
+++ src/component/Profile/RightSidebar/RightSidebar.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import styles from './RightSidebar.module.scss'
+import playImg from './../../../common/icons/play.svg'
 
 const RightSidebar = (props) => {
+
   return (
     <div className={styles.rightSideBar}>
       <div className={styles.music}>
-        Music
+        <div className={styles.sound}>
+          <input type="button" className={styles.playSound} value="Play" onClick={() => { }} id="play" />
+          <h3>Music name</h3>
+          <label htmlFor="play">
+            <img src={playImg} alt="" />
+          </label>
+          <input type="range" />
+        </div>
+        <div className={styles.sound}>
+          <input type="button" className={styles.playSound} value="Play" onClick={() => { }} id="play" />
+          <h3>Music name</h3>
+          <label htmlFor="play">
+            <img src={playImg} alt="" />
+          </label>
+          <input type="range" />
+        </div>
+        <div className={styles.sound}>
+          <input type="button" className={styles.playSound} value="Play" onClick={() => { }} id="play" />
+          <h3>Music name</h3>
+          <label htmlFor="play">
+            <img src={playImg} alt="" />
+          </label>
+          <input type="range" />
+        </div>
       </div>
     </div>
   )
