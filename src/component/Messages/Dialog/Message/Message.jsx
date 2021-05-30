8f67b9e5b58f2c08c11f602dce48b09c9d976b45
LaCuba <nikolay.romanov164@gmail.com>
2021-05-30 22:34:55 +0300

    Updated design for the Message component.

--- src/component/Messages/Dialog/Message/Message.jsx
+++ src/component/Messages/Dialog/Message/Message.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Message.module.scss'
 import avatarImg from './../../../../common/icons/avatar.svg'
+import myAvatarImg from './../../../../common/images/user-pro-img.png'
 
 const Message = (props) => {
   return (
-    <div className={props.ownerMessage ? styles.myMessage : styles.hisMessage}>
+    <div className={props.ownerMessage ? styles.myMessages : styles.hisMessages}>
       <div className={styles.avatar}>
-        <img src={avatarImg} alt="" />
+        <img src={props.ownerMessage ? myAvatarImg : avatarImg} alt="" />
       </div>
       <div className={styles.content}>
         <p>{props.bodyMessage}</p>
