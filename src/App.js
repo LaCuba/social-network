2ed448c09f781ed3e6b3d341bd377c29ebf6be60
LaCuba <nikolay.romanov164@gmail.com>
2021-05-31 18:58:43 +0300

    Added Provider with store in App component.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import Messages from './component/Messages/Messages';
 import Music from './component/Music/Music';
 import Users from './component/Users/Users';
 import 'normalize.css'
+import { Provider } from 'react-redux';
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/Store';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className={styles.app}>
-        <div className={styles.container}>
-          <Header />
-          <Route path='/profile' render={ () => <Profile /> } />
-          <Route path='/messages' render={ () => <Messages />} />
-          <Route path='/music' render={ () => <Music />} />
-          <Route path='/users' render={ () => <Users />} />
+      <Provider store={store}>
+        <div className={styles.app}>
+          <div className={styles.container}>
+            <Header />
+            <Route path='/profile' render={() => <Profile />} />
+            <Route path='/messages' render={() => <Messages />} />
+            <Route path='/music' render={() => <Music />} />
+            <Route path='/users' render={() => <Users />} />
+          </div>
         </div>
-      </div>
+      </Provider>
     </BrowserRouter>
   );
 }
